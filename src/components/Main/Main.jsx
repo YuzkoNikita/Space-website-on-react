e2a46755764ed518c42d1e190653b2407913427c
Nikko <yuzkonikita@gmail.com>
2024-09-24 17:20:08 +0200

    the Main.css file has been changed to Main.scss

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,6 @@
 import Contact from './Contact/Contact'
 import Explore from './Explore/Explore'
 import Home from './Home/Home'
-import './Main.css'
 import Travel from './Travel/Travel'
 import TravelHistory from './TravelHistory/TravelHistory'
 
