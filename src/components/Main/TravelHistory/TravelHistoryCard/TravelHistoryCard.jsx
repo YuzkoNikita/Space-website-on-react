5f5a7d83f0c036ffca43fbb5072bf3a96aaa71fe
Nikko <yuzkonikita@gmail.com>
2024-09-24 17:00:37 +0200

    the TravelHistoryCard.css file has been changed to TravelHistoryCard.scss

--- src/components/Main/TravelHistory/TravelHistoryCard/TravelHistoryCard.jsx
+++ src/components/Main/TravelHistory/TravelHistoryCard/TravelHistoryCard.jsx
@@ -1,13 +1,11 @@
-import './TravelHistoryCard.css'
-
 // eslint-disable-next-line react/prop-types
 export default function TravelHistoryCard({img, history_description}) {
   return (
     <article className="history_card">
-            <img src={img} alt="image" className="history_img" />
-            <p className="history_description">
-                {history_description}
-            </p>
-        </article>
+        <img src={img} alt="image" className="history_img" />
+        <p className="history_description">
+            {history_description}
+        </p>
+    </article>
   )
 }
